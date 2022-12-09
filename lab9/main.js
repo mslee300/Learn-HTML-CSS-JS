1ec0caf203fe1f8749c3b360e38ca81486da9367
Benjamin Lee <55467050+mslee300@users.noreply.github.com>
2022-12-08 23:14:56 -0500

    Delete lab9 directory

--- lab9/main.js
+++
@@ -1,6 +0,0 @@
-document.write("<footer><p id='foot'>Kate Wauschek © 2022. All Rights Reserved.</p></footer>");
-document.getElementById("foot").setAttribute("style", "color:red;");
-document.getElementById("hyper").setAttribute("target", "_blank");
-document.getElementById("image").removeAttribute("width");
-document.getElementById("image").setAttribute("style", "max-width:80%");
-document.getElementById("image").setAttribute("alt", "Chinchillas");
